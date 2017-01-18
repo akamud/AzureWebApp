530915c624aa14bd3d557d08441ef1f9b9dd77c6
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2017-01-17 22:11:39 -0200

    Fix Configuration Scopes

--- src/AzureWebApp/Startup.cs
+++ src/AzureWebApp/Startup.cs
@@ -45,7 +45,7 @@ namespace AzureWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration);
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 
             //loggerFactory.AddConsole((log, loglevel) => log.Contains("App") && loglevel >= LogLevel.Debug, true);
             loggerFactory.AddDebug();
